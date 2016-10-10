cd31c537921dcc6660e9b5aee024e29265b73063
Ben Brunton <ben.brunton@infinityworks.com>
2016-10-10 06:10:35 +0100

    implement insert if new user

--- project/src/data_access/user.rs
+++ project/src/data_access/user.rs
@@ -19,7 +19,16 @@ impl User {
     // insert into db if new user
     // return full user
     pub fn create_if_new(&self, user:PartUser) -> UserModel {
-        self.pool.prep_exec(r"INSERT INTO pusoy_dos.user
+        let existing = self.retrieve_user(user.clone());
+
+        match existing {
+            Some(result) => result,
+            _ => self.insert_user(user.clone())
+        }
+    }
+
+    fn insert_user(&self, user:PartUser) -> UserModel {
+        let query_result = self.pool.prep_exec(r"INSERT INTO pusoy_dos.user
                 ( name, provider_id, provider_type)
             VALUES
                 (:name, :id, :type)",
@@ -30,11 +39,44 @@ impl User {
             }).unwrap();
 
             UserModel{
-                id: 0,
+                id: query_result.last_insert_id(),
                 name: user.name.clone(),
                 provider_type: user.provider_type.clone(),
-                provider_id: user.provider_id.clone()
+                provider_id: user.provider_id.clone(),
+                creation_date: String::from("000")
             }
+    }
 
+    fn retrieve_user(&self, user:PartUser) -> Option<UserModel> {
+
+        // must be mutable because Row::take (used below) is a mutable action
+        let mut user_record = self.pool.prep_exec(r"SELECT id, 
+                                                    name, 
+                                                    creation_date, 
+                                                    provider_id, 
+                                                    provider_type
+                                FROM pusoy_dos.user
+                                WHERE id = :id",
+                            params!{
+                                "id" => user.provider_id,
+                                "type" => user.provider_type
+                            }).unwrap();
+
+        match user_record.next() {
+            Some(result) => {
+
+                let mut row = result.unwrap();
+                Some(UserModel{
+                    id: row.take("id").unwrap(),
+                    name: row.take("name").unwrap(),
+                    creation_date: row.take("creation_date").unwrap(),
+                    provider_id: row.take("provider_id").unwrap(),
+                    provider_type: row.take("provider_type").unwrap()
+                })
+
+            },
+
+            _ => None
+        }
     }
 }
--- project/src/model/user.rs
+++ project/src/model/user.rs
@@ -1,13 +1,16 @@
 
+#[derive(Clone)]
 pub struct PartUser{
     pub name: String,
     pub provider_id: String,
     pub provider_type: String
 }
 
+#[derive(Clone)]
 pub struct User{
-    pub id: u32,
+    pub id: u64,
     pub name: String,
     pub provider_id: String,
-    pub provider_type: String
+    pub provider_type: String,
+    pub creation_date: String
 }
