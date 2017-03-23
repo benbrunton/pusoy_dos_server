e29b175e9dd0c6553dabbb368ed07b6ea3a91163
Ben Brunton <ben.brunton@infinityworks.com>
2017-03-23 22:44:08 +0000

    make list of next steps

--- project/src/util/move_limit_task.rs
+++ project/src/util/move_limit_task.rs
@@ -5,4 +5,9 @@ pub fn execute(game_data: GameData) {
     let games = game_data.get_started_games_with_move_limit();
 
     info!("games: {:?}", games);
+
+    // TODO: iterate through games
+    // TODO: get latest move from event table
+    // TODO: IF move > game limit : load game and pass current user
+    // TODO: ensure to log the event to prevent cycle of passing
 }
